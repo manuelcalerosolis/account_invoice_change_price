271a0c590ed89e28f30ddf91212c468599cafca1
manuelcalerosolis <mcalero@gestool.es>
2019-10-10 12:54:34 +0200

    second commit

---
+++ models/stock_move.py
@@ -0,0 +1,29 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    cost_price = fields.Float('Cost', digits=dp.get_precision('Product Price'))
+
+class Picking(models.Model):
+    _inherit = "stock.picking"
+
+    @api.multi
+    def button_validate(self):
+        _logger.info("**** Stock picking ****")
+
+        for line in self.move_lines:
+            _logger.info(line.product_id.standard_price)
+            line.cost_price = line.product_id.standard_price
+
+        _logger.info("*"*80)
+
+        super(Picking, self).button_validate()
