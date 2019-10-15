4e82d3f75312c598cafef309ede0298a7380fdef
manuelcalerosolis <mcalero@gestool.es>
2019-10-16 01:08:09 +0200

    wip

--- models/stock_move.py
+++ models/stock_move.py
@@ -1,7 +1,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
+from odoo.exceptions import UserError
+
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -10,7 +12,8 @@ _logger = logging.getLogger(__name__)
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    cost_price = fields.Float('Previous Cost', digits=dp.get_precision('Product Price'))
+    previous_cost_price = fields.Float('Previous Cost', digits=dp.get_precision('Product Price'))
+
     list_price = fields.Float('Sales Price', compute='_compute_list_price', store=False, digits=dp.get_precision('Product Price'))
     lst_price = fields.Float('Public Price', compute='_compute_list_price', store=False, digits=dp.get_precision('Product Price'))
     standard_price = fields.Float('Cost', compute='_compute_list_price', store=False, digits=dp.get_precision('Product Price'))
@@ -20,24 +23,13 @@ class StockMove(models.Model):
     def _compute_list_price(self):
         for record in self:
             record.list_price = record.product_id.list_price
-            record.list_price = record.product_id.lst_price
+            record.lst_price = record.product_id.lst_price
             record.standard_price = record.product_id.standard_price
 
-        # list_price = fields.Float(
-        #     'Sales Price', default=1.0,
-        #     digits=dp.get_precision('Product Price'),
-        #     help="Price at which the product is sold to customers.")
-        # # lst_price: catalog price for template, but including extra for variants
-        # lst_price = fields.Float(
-        #     'Public Price', related='list_price', readonly=False,
-        #     digits=dp.get_precision('Product Price'))
-        # standard_price = fields.Float(
-        #     'Cost', compute='_compute_standard_price',
-        #     inverse='_set_standard_price', search='_search_standard_price',
-        #     digits=dp.get_precision('Product Price'), groups="base.group_user",
-        #     help="Cost used for stock valuation in standard price and as a first price to set in average/FIFO.")
-        #
-        # self.product_id.name
+    def get_search_last_purchase(self, picking_id):
+        return self.search([['product_id', '=', self.product_id.id], ['picking_id', '<>', picking_id]], limit=1,
+                           order='date desc')
+
 
 class Picking(models.Model):
     _inherit = "stock.picking"
@@ -46,12 +38,36 @@ class Picking(models.Model):
 
     @api.multi
     def button_validate(self):
-        _logger.info("**** Stock picking ****")
-
         for line in self.move_lines:
-            _logger.info(line.product_id.standard_price)
-            line.cost_price = line.product_id.standard_price
-
-        _logger.info("*" * 80)
+            search = line.get_search_last_purchase(self.id)
+            if search:
+                line.previous_purchase_date = search.create_date
+                line.previous_purchase_price = search.purchase_line_id.price_unit
+            line.previous_cost_price = line.product_id.standard_price
+            line.purchase_price = line.move_id.purchase_line_id.price_unit
 
         super(Picking, self).button_validate()
+
+        for line in self.move_lines:
+            line.cost_price = line.product_id.standard_price
+
+    @api.multi
+    def action_open_picking_prices(self):
+        self.ensure_one()
+        _logger.info(self.state)
+        if self.state != 'done':
+            raise UserError(_('The selected picking does not have validated yet. Please validate the picking and retry '))
+            return
+
+        view = self.env.ref('account_invoice_change_price.view_picking_price_form_inherit')
+
+        action = {'name': _('Picking Prices'),
+                  'view_type': 'form',
+                  'view_mode': 'tree',
+                  'res_model': 'stock.picking',
+                  'view_id': view.id,
+                  'views': [(view.id, 'form')],
+                  'type': 'ir.actions.act_window',
+                  'context': {'default_picking_id': self.id}}
+
+        return action
